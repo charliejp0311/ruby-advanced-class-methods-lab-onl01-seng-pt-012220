aadedd10f2bce9a3906bc2e2023d596a62bc83a1
Charles Pelton <charliejp0311@gmail.com>
2020-02-04 18:00:07 +0000

    Automatically backed up by Learn

--- lib/song.rb
+++ lib/song.rb
@@ -1,3 +1,4 @@
+require "pry"
 class Song
   attr_accessor :name, :artist_name
   @@all = []
@@ -8,6 +9,10 @@ class Song
 
   def save
     self.class.all << self
+  end
+
+  def self.create
+    self.new
   end
 
 end
